f3f6cf88f806d5b6612bfd78e2fbae5bb59e289b
KarlW <wennkarl@gmail.com>
2016-11-15 23:52:39 -0800

    cleaned up code, connected with mongodb

--- server.js
+++ server.js
@@ -1,46 +1,104 @@
-var express = require('express')
-    , cors = require('cors')
-    , app = express();
+var express = require('express');
+var cors = require('cors');
+var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var path = require('path');
 var player = require('play-sound')(opts = { timeout: 300 });
-//var test = require('./sockets/base')(io);
+var mongo = require('mongodb');
+//var session = require('express-session');
+var bcrypt = require('bcryptjs');
+var mongoose = require('mongoose');
+
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/auth');
+//mongodb schema
+var Schema = mongoose.Schema;
+var UserDetail = new Schema({
+		username: String,
+		password: String
+    }, {
+		collection: 'userInfo'
+    });
+var UserDetails = mongoose.model('userInfo', UserDetail);
+
+
+//Authentication
+passport.use(new LocalStrategy(function(username, password, done) {
+	process.nextTick(function() {
+    	UserDetails.findOne({
+			'username': username, 
+    	}, function(err, user) {
+			if (err) { return done(err);}
+	  		if (!user) { return done(null, false);}	
+	  		if (user.password != password) { return done(null, false);}
+	  	return done(null, user);
+    	});
+  	});
+}));
 
 
 app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors());
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-    console.log("successfully logged in");
-    res.redirect('/dashboard/' + req.user.username);
-});
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'passwd',
+    session: false
+},
+	function(username, password, done) {
+    // ...
+	}
+));
 
-
-app.get('/register', function(req, res){
-		res.sendFile(path.join(__dirname + '/public/register.html'));
-});
 
 app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.get('/dashboard', function(req, res){
-        res.sendFile(path.join(__dirname + '/public/dashboard.html'));
+    res.sendFile(path.join(__dirname + '/public/dashboard.html'));
 });
 
+app.post('/login', passport.authenticate('local', { failureRedirect: '/loginFailure' }),
+	function(req, res) {
+    	res.redirect('/');
+});
 
+app.get('/loginSuccess', function(req, res){
+	res.send("Successfully logged in");
+});
+
+app.get('/loginFailure', function(req, res){
+	res.send("Failure to logged in");
+});
+
+app.get('/logout', function(req, res){
+	res.redirect('/');
+});
+
+app.get('/register', function(req, res){
+	res.sendFile(path.join(__dirname + '/public/register.html'));
+});
+
+app.post('/register', function(req, res){
+	res.json(req.body);
+});
+
+/*
 io.on('connection', function(socket){
   console.log('a user connected');
 });
+*/
 
 app.post('/', function(req, res) {
 	var URLS = req.body;
@@ -48,6 +106,14 @@ app.post('/', function(req, res) {
 	console.log(url);
 	player.play( __dirname + '/public/assets/soundclips/' + url + '.mp3', function(err){if (err) throw err});
     res.status(200).send((url).toString());
+});
+
+
+//mongodb connection test
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected");
 });
 
 app.listen(3000, function () {
