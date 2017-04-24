4a356f80a81fa7cedcdde50cdfd023c9bf6f0e56
KarlW <wennkarl@gmail.com>
2017-04-24 07:21:16 -0700

    added bcrypt for pw hashing

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ const path = require('path');
 const player = require('play-sound')(opts = { timeout: 300 });
 const mongo = require('mongodb');
 //const session = require('express-session');
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const session = require('client-sessions');
 const app = express();
+
+
+/**
+ * Middleware functions
+ */
 
 app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
@@ -43,27 +48,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
+app.use(session({
+  cookieName: 'session',
+  secret: 'random_string_goes_here',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
 
+
+//Configurations
 app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 
-//const routes = require('/app/routes');
-//app.use('/', routes);
+/**
+ * Mongoose Connection
+ */
 
-
-//connect to mongodb
 mongoose.connect('mongodb://localhost/auth');
 //mongodb schema
 var Schema = mongoose.Schema;
-var UserDetail = new Schema({
+var ObjectId = Schema.ObjectId;
+var userSchema = new Schema({
+		first_name: String,
+		last_name: String,
+		email: String,
 		username: String,
-		password: String
-    }, {
-		collection: 'userInfo'
+		auth: String
     });
-var UserDetails = mongoose.model('userInfo', UserDetail);
+    
+var Users = mongoose.model('Users', userSchema);
 
 
 //Authentication
@@ -81,6 +96,8 @@ passport.use(new LocalStrategy(function(username, password, done) {
 }));
 passport.use(new LocalStrategy({
     usernameField: 'email',
+    firstName: 'firstname',
+    lastName: 'lastname',
     passwordField: 'passwd',
     session: false
 },
@@ -90,6 +107,8 @@ passport.use(new LocalStrategy({
 ));
 
 
+
+//Routes
 
 app.get('/', function(req, res){
     res.render('index.html');
@@ -121,8 +140,10 @@ app.get('/register', function(req, res){
 });
 
 app.post('/register', function(req, res){
-		console.log(req.body);
+	
+	//console.log(req.body);
 
+	var userName = req.body.userName;
 	var firstName = req.body.firstName;
 	var lastName = req.body.lastName;
 	var email = req.body.email;
@@ -142,8 +163,19 @@ app.post('/register', function(req, res){
 	}else{
 		res.send(false);
 		//session start code
+			//if passwords match and requirements are met, hash password here
+		if(password===password2){
+			bcrypt.hash(password, 10, function(err, hash) {
+				// Store hash in database
+			})
+		}
 	}
 	//res.json(req.body);
+	
+	Users.find({}, function(err,users){
+		if (err) throw err;
+		console.log(users);
+	})	
 });
 
 /*
@@ -161,12 +193,58 @@ app.post('/', function(req, res) {
 });
 
 
-//mongodb connection test
+
+
+/**
+ * Mongoose connections test
+ */
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("connected");
+  console.log("mongooose connected");
 });
+
+process.on('exit', function(code) {
+mongoose.disconnect();
+});
+
+
+/**
+ * Model Logic
+ */
+
+function find(collec, query, callback) {
+    mongoose.connection.db.collection(collec, function (err, collection) {
+    collection.find(query).toArray(callback);
+    });
+}
+
+
+
+/**
+ * App Logic
+ */
+
+function checkpassword(pwtest, pwhash){
+	bcrypt.compare(pwtest, pwhash, function(err, res) {
+		if(res) {
+		// Passwords match - returns true
+		//console.log("Matches=>" + res);
+		//create session variable
+		req.session.user = user;  //loads cookie
+  		} else {
+  		// Passwords don't match -returns false
+  		//console.log("Dont Match=>" + res);
+  		
+  		} 
+	});
+}
+
+
+/**
+ * Express Connection
+ */
 
 app.listen(3000, function () {
   console.log('Vader  listening on port 3000!');
@@ -174,10 +252,12 @@ app.listen(3000, function () {
 
 
 
+/**
+ * To Do's
+ */
+
 
 //uv4l and webrtc for video
-
-
 //stream video and audio to website from webcam/mic on raspberry pi
 //create session - create a cue for users
 //make session end after 3 minutes if somebody in cude
